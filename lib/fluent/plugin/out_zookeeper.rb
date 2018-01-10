2b5d8b904f939c0a837aaa8f54aa96d5f0d25052
Anton Tikhomirov <anton.tikhomirov@cdnetworks.com>
2018-01-10 10:57:31 +0900

    Remove unused objspace dependency

--- lib/fluent/plugin/out_zookeeper.rb
+++ lib/fluent/plugin/out_zookeeper.rb
@@ -13,8 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require "fluent/plugin/output"
-require 'objspace'
+require 'fluent/plugin/output'
 require 'oj'
 require 'zookeeper'
 
